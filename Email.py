c6e982b0d9bd1b01c297af7ad3543573ea78da09
Arjun Ajith <arjuunajith@gmail.com>
2020-06-14 12:48:10 +0530

    Create Email.py

---
+++ Email.py
@@ -0,0 +1,21 @@
+import smtplib
+from email.mime.text import MIMEText
+
+body = "This is a test email.How are you"
+
+msg = MIMEText(body)
+msg['From'] = "arjuunajith@gmail.com"
+msg['To'] = "arjun_b160140ec@nitc.ac.in"
+msg['Subject'] = "Sample Mail"
+
+server = smtplib.SMTP('localhost')
+
+server.starttls()
+
+server.login("arjuunajith@gmail.com","")
+
+server.send_message(msg)
+
+print("Mail sent")
+
+server.quit()
